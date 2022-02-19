d9399ea4c817b96cb7060fe5383c37eede33e237
cyl3x <cyl3x@posteo.de>
2022-02-19 20:11:09 +0100

    add drift as button

---
+++ assets/joystick/button.gd
@@ -0,0 +1,70 @@
+extends Control
+
+var _touch_index = -1
+
+export(Color) var default_color = Color(1.0,1.0,1.0,0.2)
+export(Color) var pressed_color = Color(1.0,1.0,1.0,0.3)
+export(int, 50, 2000, 1) var double_click_interval = 200
+
+var first_pressed_time = 0
+var last_pressed_time = 0
+var double_clicked = false
+
+export(PoolStringArray) var on_single_click = PoolStringArray()
+export(PoolStringArray) var on_double_click = PoolStringArray()
+
+enum ButtonMode {FIXED, DYNAMIC}
+
+export(ButtonMode) var button_mode := ButtonMode.FIXED
+
+onready var texture = $texture
+onready var texture_default_position = texture.rect_position
+
+func _ready():
+	texture.modulate = default_color
+
+func _input(event) -> void:
+	if not visible or not event is InputEventScreenTouch: return
+	
+	if event.pressed:
+		if _is_point_inside_area(event.position) and _touch_index == -1:
+			if button_mode is ButtonMode.DYNAMIC:
+				_move_base(event.position)
+			_touch_index = event.index
+			first_pressed_time = OS.get_ticks_msec()
+			texture.modulate = pressed_color
+			
+			_press_actions(on_single_click)
+			
+			if first_pressed_time - last_pressed_time <= double_click_interval:
+				_press_actions(on_double_click)
+				double_clicked = true
+				
+			get_tree().set_input_as_handled()
+	elif event.index == _touch_index:
+		_release_actions(on_single_click)
+		texture.modulate = default_color
+		_touch_index = -1
+		_release_actions(on_double_click)
+		double_clicked = false
+		if OS.get_ticks_msec() - first_pressed_time <= double_click_interval:
+			last_pressed_time = OS.get_ticks_msec()
+		if button_mode is ButtonMode.DYNAMIC:
+			texture.rect_position = texture_default_position
+		get_tree().set_input_as_handled()
+
+func _move_base(new_position: Vector2) -> void:
+	texture.rect_global_position = new_position - texture.rect_pivot_offset * get_global_transform_with_canvas().get_scale()
+	
+func _press_actions(buttons:PoolStringArray):
+	for button in buttons:
+		Input.action_press(button)
+		
+func _release_actions(buttons:PoolStringArray):
+	for button in buttons:
+		Input.action_release(button)
+
+func _is_point_inside_area(point: Vector2) -> bool:
+	var x: bool = point.x >= rect_global_position.x and point.x <= rect_global_position.x + (rect_size.x * get_global_transform_with_canvas().get_scale().x)
+	var y: bool = point.y >= rect_global_position.y and point.y <= rect_global_position.y + (rect_size.y * get_global_transform_with_canvas().get_scale().y)
+	return x and y
