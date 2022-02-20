1a13f68daba1b63f66715abbded8039db209f3eb
cyl3x <cyl3x@posteo.de>
2022-02-20 21:52:22 +0100

    dynamic view distance

--- cars/cam.gd
+++ cars/cam.gd
@@ -3,7 +3,10 @@ extends InterpolatedCamera
 func _process(_delta):
 	var fps = Engine.get_frames_per_second()
 	
-	if fps >= 60 and !far > 500:
-		far = far+1
-	if fps < 60 and !far < 100: #!far < 100
-		far = far-1
+	if Players.view_distance == 100:
+		if fps >= 60 and !far > 500:
+			far = far+1
+		if fps < 60 and !far < 100:
+			far = far-1
+	else:
+		far = Players.view_distance
