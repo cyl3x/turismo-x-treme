d36a44c509bfb8592e99a2de0b39c1ab8325ac15
cyl3x <michel-bade@posteo.de>
2022-03-08 13:43:17 +0100

    add config file

---
+++ server/config.gd
@@ -0,0 +1,27 @@
+extends Node
+
+const public_config_paths = {
+	"windows": "C:/Users/Public/Documents/Racing-Game",
+	"X11":  "~/.config/Racing-Game",
+	"OSX":  "~/Library/Application Support/Racing-Game",
+	"Android":  "/data/data/org.oekel.racinggame/files",
+} 
+const file_name = "public_config.json"
+onready var real_path = "user://" + public_config_paths[OS.get_name()]
+
+var config = {}
+
+func _ready():
+	#var dir = Directory.new()
+	
+	#dir.make_dir_recursive(real_path)
+	#dir.open(real_path)
+	
+	#var file = File.new()
+	
+	#file.open(real_path + "/" + file_name, File.READ_WRITE)
+	
+	#if file.file_exists(real_path + "/" + file_name):
+		#config = JSON.parse(file.get_as_text())
+	#else:
+		pass
